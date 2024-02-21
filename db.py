89651b4b534266d61c366473301c4c0ddc547bbe
Jordan Manus <94319835+jordan-manus@users.noreply.github.com>
2024-02-21 16:14:47 -0500

    added validation to endpoints and updated dockerfile.

---
+++ db.py
@@ -0,0 +1,13 @@
+stores = {}
+items = {
+    1: {
+        "name": "Chair",
+        "price": 17.99,
+        "store_id": 1
+    },
+    2: {
+        "name": "Table",
+        "price": 18.50,
+        "store_id": 2
+    }
+}
